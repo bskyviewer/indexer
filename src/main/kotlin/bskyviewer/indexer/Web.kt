7e9e6252343a39ca8915d8320e68b11e34b27cb2
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2025-06-22 11:43:44 +0700

    post mapping

--- src/main/kotlin/bskyviewer/indexer/Web.kt
+++ src/main/kotlin/bskyviewer/indexer/Web.kt
@@ -1,6 +1,8 @@
 package bskyviewer.indexer
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
@@ -10,9 +12,18 @@ class Web(val index: Index) {
     @ResponseBody
     @GetMapping("/")
     fun index(
-        @RequestParam(defaultValue = "[* TO *]") q: String,
+        @RequestParam(defaultValue = "") q: String,
         @RequestParam(defaultValue = "desc") sort: List<String>,
         @RequestParam(defaultValue = "100") limit: Int,
         @RequestParam(required = false) dids: List<String>?,
     ) = index.search(q, limit, sort, dids ?: emptyList())
+
+    @ResponseBody
+    @PostMapping("/")
+    fun indexer(
+        @RequestParam(defaultValue = "") q: String,
+        @RequestParam(defaultValue = "desc") sort: List<String>,
+        @RequestParam(defaultValue = "100") limit: Int,
+        @RequestBody dids: List<String>,
+    ) = index.search(q, limit, sort, dids)
 }
