835f101ce268ea5fae9d0302df40fdcb75239608
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2025-06-24 17:12:48 +0700

    catch and retry

--- src/main/kotlin/bskyviewer/indexer/Listener.kt
+++ src/main/kotlin/bskyviewer/indexer/Listener.kt
@@ -1,12 +1,17 @@
 package bskyviewer.indexer
 
 import app.bsky.jetstream.SubscribeQueryParams
+import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.reactor.mono
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 import sh.christian.ozone.api.Nsid
 import sh.christian.ozone.jetstream.JetstreamApi
+
+private val logger = KotlinLogging.logger {}
+val params = SubscribeQueryParams(wantedCollections = listOf(Nsid("app.bsky.feed.post")))
 
 @Component
 class Listener(val messageQueue: MessageQueue) {
@@ -15,8 +20,10 @@ class Listener(val messageQueue: MessageQueue) {
     @EventListener(ApplicationReadyEvent::class)
     fun startup() {
         mono {
-            client.subscribe(SubscribeQueryParams(wantedCollections = listOf(Nsid("app.bsky.feed.post"))))
-                .also { it.collect(messageQueue::emit) }
+            client.subscribe(params).catch {
+                logger.error(it) { "error in subscription" }
+                startup()
+            }.collect(messageQueue::emit)
         }.subscribe()
     }
 }
