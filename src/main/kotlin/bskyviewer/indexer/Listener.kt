4499b7790c427f63321488726d0c05c61d1070d5
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2025-06-24 17:13:53 +0700

    suspend fun

--- src/main/kotlin/bskyviewer/indexer/Listener.kt
+++ src/main/kotlin/bskyviewer/indexer/Listener.kt
@@ -3,7 +3,6 @@ package bskyviewer.indexer
 import app.bsky.jetstream.SubscribeQueryParams
 import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.reactor.mono
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
@@ -18,12 +17,10 @@ class Listener(val messageQueue: MessageQueue) {
     val client = JetstreamApi()
 
     @EventListener(ApplicationReadyEvent::class)
-    fun startup() {
-        mono {
-            client.subscribe(params).catch {
-                logger.error(it) { "error in subscription" }
-                startup()
-            }.collect(messageQueue::emit)
-        }.subscribe()
+    suspend fun startup() {
+        client.subscribe(params).catch {
+            logger.error(it) { "error in subscription" }
+            startup()
+        }.collect(messageQueue::emit)
     }
 }
