a4978ba973f38ba5bc65aaa3f84432a80cf03c82
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2025-07-05 20:32:24 +0700

    produce json

--- src/main/kotlin/bskyviewer/indexer/web/Api.kt
+++ src/main/kotlin/bskyviewer/indexer/web/Api.kt
@@ -6,12 +6,14 @@ import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
 class Api(val index: Index, val analyser: Analyser) {
 
-    @GetMapping("/langs", produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping("/langs")
     fun langs() = mapOf("seen" to index.langs, "known" to analyser.byLang.keys)
 
     @GetMapping("/size")
