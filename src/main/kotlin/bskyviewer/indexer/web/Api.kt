749e7f903d91d95f4422fe96307b227f5d05b9a9
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2025-07-12 11:45:07 +0700

    allow cors

--- src/main/kotlin/bskyviewer/indexer/web/Api.kt
+++ src/main/kotlin/bskyviewer/indexer/web/Api.kt
@@ -3,12 +3,14 @@ package bskyviewer.indexer.web
 import bskyviewer.indexer.Index
 import bskyviewer.indexer.lucene.Analyser
 import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin
 @RestController
 @RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
 class Api(val index: Index, val analyser: Analyser) {
